46f00ae8a58e39658a895553828f674fd35c2d57
Jessé Figueró <figuerojesse@gmail.com>
2022-10-21 20:51:42 -0300

    Initial commit

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,52 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController(
+    val customerService: CustomerService
+) {
+    // utilizado para um controlador endpoint
+    @GetMapping
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        return customerService.getAll(name);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        customerService.create(customer);
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel? {
+        return customerService.getCustomer(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customerService.update(id, customer);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customerService.delete(id);
+    }
+}
